5f305660d34320fb3adbfd7a1539f8c50840f5bf
Angeltapes <kingrober@gmail.com>
2024-10-22 14:08:59 +0100

    Got data defaults working

--- app/data/session-data-defaults.js
+++ app/data/session-data-defaults.js
@@ -1,7 +1,9 @@
 let people = require('./people.json')
 module.exports = {
-  people
+  people,
 
   // Insert values here
-
+  // Access these using the format {{ data['first-name'] or "First name not given" }}
+ 'first-name': 'Boab',
+  'over-18': 'yes'
 }
