251d506174b1fe2392389cb955086463893bc661
Angeltapes <bobby.king@madetech.com>
2025-03-15 08:50:35 +0000

    Tidied up passing data page

--- app/data/session-data-defaults-broken.js
+++ app/data/session-data-defaults-broken.js
@@ -1,6 +1,8 @@
 let people = require('./people.json')
-export default {
+let myData = require('./my-data.csv') //these imports no longer work
+module.exports = {
   people,
+  myData,
   // Insert values here
   // Access these using the format {{ data['first-name'] or "First name not given" }}
  'first-name': 'Boab',
